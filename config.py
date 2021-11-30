6358814b9105bdbc9ce42414a89b051edbbeacf7
bmarsh9 <bmarshall735@gmail.com>
2021-11-30 01:34:18 +0000

    updates

--- config.py
+++ config.py
@@ -45,8 +45,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or \
-        "postgresql://db12:db12@localhost/db12"
-#        "postgresql://db1:db1@postgres_db/db1"
+        "postgresql://db1:db1@postgres_db/db1"
 
 class TestingConfig(Config):
     TESTING = True
